dccf439a40555c45e85c0e7f5fd27b049d05e353
Nandor Devai <mail@nandordevai.net>
2020-12-04 19:34:24 +0100

    first commit

---
+++ Assets/DLA.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DLA : MonoBehaviour
+{
+    public int numWalkers;
+    public GameObject dotPrefab;
+
+    List<Walker> walkers = new List<Walker>();
+
+    void Start()
+    {
+        for (var i = 0; i < numWalkers; i++) {
+            Walker walker = gameObject.AddComponent<Walker>();
+            walker.Draw(dotPrefab);
+            walkers.Add(walker);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Walker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walker : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    public void Draw(GameObject dot)
+    {
+        Instantiate(dot, new Vector3(Random.value * 20 - 10, Random.value * 20, 0), Quaternion.identity);
+    }
+
+    void Update()
+    {
+
+    }
+}
