0e912cf6b301200b38d061b7a976cbbc2ada3005
Nandor Devai <mail@nandordevai.net>
2020-12-04 22:05:09 +0100

    add walk & initial tree item

--- Assets/DLA.cs
+++ Assets/DLA.cs
@@ -6,20 +6,34 @@ public class DLA : MonoBehaviour
 {
     public int numWalkers;
     public GameObject dotPrefab;
-
-    List<Walker> walkers = new List<Walker>();
+    readonly List<Walker> walkers = new List<Walker>();
+    readonly List<Walker> tree = new List<Walker>();
+    GameObject treeContainer;
+    GameObject walkerContainer;
 
     void Start()
     {
-        for (var i = 0; i < numWalkers; i++) {
+        walkerContainer = GameObject.Find("Walkers");
+        for (var i = 0; i < numWalkers; i++)
+        {
             Walker walker = gameObject.AddComponent<Walker>();
-            walker.Draw(dotPrefab);
+            walker.Draw(dotPrefab, walkerContainer);
+            walker.SetRandomPosition();
             walkers.Add(walker);
         }
+        treeContainer = GameObject.Find("Tree");
+        Walker trunk = gameObject.AddComponent<Walker>();
+        trunk.transform.position = new Vector3(0, 0, 0);
+        trunk.Draw(dotPrefab, treeContainer);
+        tree.Add(trunk);
     }
 
     void Update()
     {
-        
+        for (var i = walkers.Count - 1; i >= 0; i--)
+        {
+            Walker w = walkers[i];
+            w.Walk();
+        }
     }
 }
--- Assets/Walker.cs
+++ Assets/Walker.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class Walker : MonoBehaviour
 {
+    GameObject dot;
+
     void Start()
     {
-
     }
 
-    public void Draw(GameObject dot)
+    public void Draw(GameObject dotP, GameObject container)
     {
-        Instantiate(dot, new Vector3(Random.value * 20 - 10, Random.value * 20, 0), Quaternion.identity);
+        dot = Instantiate(dotP, new Vector3(0, 0, 0), Quaternion.identity);
+        dot.transform.SetParent(container.transform);
+    }
+
+    public void SetRandomPosition()
+    {
+        dot.transform.position = new Vector3(Random.value * 20 - 10, Random.value * 20, 0);
+    }
+
+    public void Walk()
+    {
+        dot.transform.position += new Vector3(Random.value / 10 - 0.05f, -Random.value / 10, 0);
     }
 
     void Update()
